90cb8bb98d3d1b71fba8fed97099bb8b7fc3f658
afthah <afthaherooth1@gmail.com>
2025-12-05 23:12:11 +0530

    Updated project

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello),
-    path('tasks/', views.get_tasks),
-    path('tasks/add/', views.create_task),
-    path('tasks/<int:id>/', views.update_task),
-    path('tasks/<int:id>/delete/', views.delete_task),
+    path('hello/', views.hello, name='hello'),
+    
+    # Task CRUD API
+    path('tasks/', views.get_tasks, name='get_tasks'),
+    path('tasks/add/', views.create_task, name='create_task'),
+    path('tasks/<int:id>/', views.update_task, name='update_task'),
+    path('tasks/<int:id>/delete/', views.delete_task, name='delete_task'),
 ]
